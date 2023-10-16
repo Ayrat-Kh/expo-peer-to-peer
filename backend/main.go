4430c776ba4d785b36e3dadd192f1be47118b2ff
Airat Khsiamiev <aira3t@gmail.com>
2023-10-16 15:48:32 +0200

    Update data

--- backend/main.go
+++ backend/main.go
@@ -8,26 +8,13 @@ import (
 
 var addr = flag.String("addr", "localhost:5000", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	hub := NewHub()
 	go hub.Run()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", serveHome)
+
 	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("ws")
 		ServeWs(hub, w, r)
@@ -40,6 +27,4 @@ func main() {
 	if err != nil {
 		log.Fatalln("Listen and server", err)
 	}
-
-	log.Println("Listen and server")
 }
